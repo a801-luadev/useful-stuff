4b1301dfa0b668793a6ce04820dbfe9becd76ce2
Lautenschlager <tai.romani@hotmail.com>
2020-01-22 20:28:30 -0300

    improve performance and split commas

--- mapGrounds.lua
+++ mapGrounds.lua
@@ -1,4 +1,37 @@
-local gsub = string.gsub
+local find, gmatch = string.find, string.gmatch
+
+local split = function(str, delimiter, f, isPlain)
+	local out, index = { }, 0
+
+	local n, i, j = 1
+	while true do
+		i, j = find(str, delimiter, n, isPlain)
+		if not i then break end
+
+		index = index + 1
+		out[index] = sub(str, n, i - 1)
+		if f then
+			out[index] = f(out[index])
+		end
+
+		n = j + 1
+	end
+
+	index = index + 1
+	out[index] = sub(str, n)
+	if f then
+		out[index] = f(out[index])
+	end
+
+	return out
+end
+
+local getValue = function(value)
+	if value == '' then
+		return 0
+	end
+	return tonumber(value) or value
+end
 
 local grounds = { }
 eventNewGame = function()
@@ -9,12 +42,21 @@ eventNewGame = function()
 		xml = xml.xml
 
 		local groundIndex = 0
-		gsub(xml, "<S (.-)/>", function(groundData)
+		for groundData in gmatch(xml, "<S (.-)/>") do
 			groundIndex = groundIndex + 1
 			grounds[groundIndex] = { }
-			gsub(groundData, "([%w-_]+)=([\"'])(.-)%2", function(attributeName, _, value)
-				grounds[groundIndex][attributeName] = (tonumber(value) or value)
-			end)
-		end)
+			for attributeName, _, value in gmatch(groundData, "([%w-_]+)=([\"'])(.-)%2") do
+				grounds[groundIndex][attributeName] = (tonumber(value) or (find(value, ',', 1, true) and split(value, ',', getValue, true)) or value)
+			end
+		end
 	end
 end
+
+--[[ Table 'grounds' gets updated on every new game
+
+Ex:
+
+grounds[1].X -- Horizontal position (X) of the first ground (z-index: 0)
+grounds[1].P[5] -- Angle of the first ground
+
+]]
