cf6e65ab847609c920cf0e7075cbd4805223aafe
Lautenschlager <tai.romani@hotmail.com>
2020-01-27 14:54:07 -0300

    make it more readable

--- mapGrounds.lua
+++ mapGrounds.lua
@@ -26,30 +26,39 @@ local split = function(str, delimiter, f, isPlain)
 	return out
 end
 
-local getValue = function(value)
-	if value == '' then
+local grounds, getGrounds = { } -- Could be a function, but its calls are expensive
+do
+	local getValue = function(value)
+		if value == '' then
+			return 0
+		end
+		return tonumber(value) or value
+	end
+
+	getGrounds = function()
+		grounds = { }
+
+		local xml = tfm.get.room.xmlMapInfo
+		if xml then
+			xml = xml.xml
+
+			local groundIndex = 0
+			for groundData in gmatch(xml, "<S (.-)/>") do
+				groundIndex = groundIndex + 1
+				grounds[groundIndex] = { }
+				for attributeName, _, value in gmatch(groundData, "([%w-_]+)=([\"'])(.-)%2") do
+					grounds[groundIndex][attributeName] = (tonumber(value) or (find(value, ',', 1, true) and split(value, ',', getValue, true)) or value)
+				end
+			end
+			return groundIndex
+		end
 		return 0
 	end
-	return tonumber(value) or value
 end
 
-local grounds = { }
 eventNewGame = function()
-	grounds = { }
-
-	local xml = tfm.get.room.xmlMapInfo
-	if xml then
-		xml = xml.xml
-
-		local groundIndex = 0
-		for groundData in gmatch(xml, "<S (.-)/>") do
-			groundIndex = groundIndex + 1
-			grounds[groundIndex] = { }
-			for attributeName, _, value in gmatch(groundData, "([%w-_]+)=([\"'])(.-)%2") do
-				grounds[groundIndex][attributeName] = (tonumber(value) or (find(value, ',', 1, true) and split(value, ',', getValue, true)) or value)
-			end
-		end
-	end
+	local hasGrounds = getGrounds() > 0 -- Gets the map grounds. 'hasGrounds' is false when 0 grounds are detected (vanilla, empty maps)
+	-- TODO: Your eventNewGame code
 end
 
 --[[ Table 'grounds' gets updated on every new game
@@ -58,5 +67,12 @@ Ex:
 
 grounds[1].X -- Horizontal position (X) of the first ground (z-index: 0)
 grounds[1].P[5] -- Angle of the first ground
+
+Sample:
+
+if hasGrounds then
+	print(grounds[1].X)
+end
+
 
 ]]
