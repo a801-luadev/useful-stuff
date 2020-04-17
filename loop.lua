a67fbf294209ef003f7cd4670492cb3986d9d42b
Lautenschlager <tai.romani@hotmail.com>
2020-04-17 00:21:54 -0300

    Create loop.lua

---
+++ loop.lua
@@ -0,0 +1,14 @@
+local loop = function(f, ticks, ...)
+	local timers, index = { }, 0
+	local addTimer = function(_, ...)
+		index = index + 1
+		timers[index] = system.newTimer(f, 1000, true, ...)
+	end
+
+	local seconds = 1000 / ticks
+	for timer = 0, 1000 - seconds, seconds do
+		system.newTimer(addTimer, 1000 + timer, false, ...)
+	end
+
+	return timers
+end
